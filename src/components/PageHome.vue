1049dc61d5ff9f2ae5207f827a1475d54330914e
Madhusha Prasad <50085447+MadhushaPrasad@users.noreply.github.com>
2022-12-05 11:58:03 +0530

    use v-for for display array element details

--- src/components/PageHome.vue
+++ src/components/PageHome.vue
@@ -1,12 +1,29 @@
 <template>
   <div>
     <h1>Hello Hee</h1>
+    <div v-for="thread in threads" :key="thread.id">
+      <h2>{{ thread.title }}</h2>
+      <div v-for="postId in thread.posts" :key="postId">
+        <p>{{ users.find(u => u.id === posts.find(p => p.id === postId).userId).name }}</p>
+        <p>{{ posts.find(p => p.id === postId).text }}</p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import sourceData from '@/data.json'
+
 export default {
   name: "PageHome"
+  ,
+  data() {
+    return {
+      threads: sourceData.threads,
+      posts: sourceData.posts,
+      users: sourceData.users
+    }
+  }
 }
 </script>
 
